5e3089f67db1b9a2643e3db60333216fd15f07ef
ihanyang <h513915503@163.com>
2016-03-25 17:42:15 +0800

    继续完善

--- server.js
+++ server.js
@@ -4,31 +4,41 @@ var app = express()
 var path = require("path")
 var fs = require("fs")
 
-app.get("*", (req, res) => {
-	var paths = path.join(__dirname, req.path)
+// app.get("*", (req, res) => {
+// 	var paths = path.join(__dirname, req.path)
 
-	if (req.path === "/") {
-		res.set("html")
-		fs.createReadStream("./index.html").pipe(res)
+// 	if (req.path === "/") {
+// 		res.set("html")
+// 		fs.createReadStream("./index.html").pipe(res)
 
-		return
+// 		return
+// 	}
+
+// 	fs.exists(paths, (exists) => {
+// 		if (exists) {
+// 			res.set({
+// 				"Content-Type": mime.lookup(paths)
+// 			})
+// 			fs.createReadStream(paths).pipe(res)
+
+// 			return
+// 		}
+
+// 		if (! exists) {
+// 			res.set("html")
+// 			fs.createReadStream("./index.html").pipe(res)
+// 		}
+// 	})
+// })
+
+app.use(express.static(__dirname))
+
+app.use(function (req, res, next) {
+	if (req.path !== "/" || ! req.path.includes("dist")) {
+		res.sendFile(path.join(__dirname, "/index.html"))
 	}
 
-	fs.exists(paths, (exists) => {
-		if (exists) {
-			res.set({
-				"Content-Type": mime.lookup(paths)
-			})
-			fs.createReadStream(paths).pipe(res)
-
-			return
-		}
-
-		if (! exists) {
-			res.set("html")
-			fs.createReadStream("./index.html").pipe(res)
-		}
-	})
+	next()
 })
 
 app.listen(process.env.PORT || 8080)
