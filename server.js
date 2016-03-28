55a262e41aa58cf83d2e82e81edaeb8c69e4ef13
ihanyang <h513915503@163.com>
2016-03-28 12:17:40 +0800

    modify

--- server.js
+++ server.js
@@ -1,44 +1,14 @@
 var express = require("express")
-var mime = require("mime")
 var app = express()
-var path = require("path")
-var fs = require("fs")
-
-// app.get("*", (req, res) => {
-// 	var paths = path.join(__dirname, req.path)
-
-// 	if (req.path === "/") {
-// 		res.set("html")
-// 		fs.createReadStream("./index.html").pipe(res)
-
-// 		return
-// 	}
-
-// 	fs.exists(paths, (exists) => {
-// 		if (exists) {
-// 			res.set({
-// 				"Content-Type": mime.lookup(paths)
-// 			})
-// 			fs.createReadStream(paths).pipe(res)
-
-// 			return
-// 		}
-
-// 		if (! exists) {
-// 			res.set("html")
-// 			fs.createReadStream("./index.html").pipe(res)
-// 		}
-// 	})
-// })
 
 app.use(express.static(__dirname))
 
-app.use(function (req, res, next) {
-	if (req.path !== "/" || ! req.path.includes("dist")) {
-		res.sendFile(path.join(__dirname, "/index.html"))
-	}
+// app.use(function (req, res, next) {
+// 	if (req.path !== "/" || ! req.path.includes("dist")) {
+// 		res.sendFile(path.join(__dirname, "/index.html"))
+// 	}
 
-	next()
-})
+// 	next()
+// })
 
 app.listen(process.env.PORT || 8080)
